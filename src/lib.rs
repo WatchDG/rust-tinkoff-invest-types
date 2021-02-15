2a7f149a0f311635d4639cf67f172c4d0b67daa1
WatchDG <baranov@w3m.co>
2021-02-15 22:18:39 +0300

    rename types and add Serialize

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,6 @@
-use serde::Deserialize;
+use serde::{Deserialize, Serialize};
 
-#[derive(Deserialize, Debug)]
+#[derive(Serialize, Deserialize, Debug)]
 pub enum Currency {
     RUB,
     USD,
@@ -13,7 +13,7 @@ pub enum Currency {
     TRY,
 }
 
-#[derive(Deserialize, Debug)]
+#[derive(Serialize, Deserialize, Debug)]
 pub enum InstrumentType {
     Stock,
     Currency,
@@ -21,13 +21,13 @@ pub enum InstrumentType {
     Etf,
 }
 
-#[derive(Deserialize, Debug)]
+#[derive(Serialize, Deserialize, Debug)]
 pub enum OperationType {
     Buy,
     Sell,
 }
 
-#[derive(Deserialize, Debug)]
+#[derive(Serialize, Deserialize, Debug)]
 pub enum OrderStatus {
     New,
     PartiallyFill,
@@ -40,25 +40,25 @@ pub enum OrderStatus {
     PendingNew,
 }
 
-#[derive(Deserialize, Debug)]
+#[derive(Serialize, Deserialize, Debug)]
 pub enum OrderType {
     Limit,
     Market,
 }
 
-#[derive(Deserialize, Debug)]
+#[derive(Serialize, Deserialize, Debug)]
 pub enum BrokerAccountType {
     Tinkoff,
     TinkoffIis,
 }
 
-#[derive(Deserialize, Debug)]
+#[derive(Serialize, Deserialize, Debug)]
 pub struct MoneyAmount {
     pub currency: Currency,
     pub value: f64,
 }
 
-#[derive(Deserialize, Debug)]
+#[derive(Serialize, Deserialize, Debug)]
 pub struct UserAccount {
     #[serde(rename(serialize = "brokerAccountId", deserialize = "brokerAccountId"))]
     pub broker_account_id: String,
@@ -66,12 +66,12 @@ pub struct UserAccount {
     pub broker_account_type: BrokerAccountType,
 }
 
-#[derive(Deserialize, Debug)]
-pub struct UserAccounts {
+#[derive(Serialize, Deserialize, Debug)]
+pub struct UserAccountsPayload {
     pub accounts: Vec<UserAccount>,
 }
 
-#[derive(Deserialize, Debug)]
+#[derive(Serialize, Deserialize, Debug)]
 pub struct MarketInstrument {
     pub figi: String,
     pub ticker: String,
@@ -85,12 +85,12 @@ pub struct MarketInstrument {
     pub type_: InstrumentType,
 }
 
-#[derive(Deserialize, Debug)]
-pub struct MarketInstruments {
+#[derive(Serialize, Deserialize, Debug)]
+pub struct MarketInstrumentsPayload {
     pub instruments: Vec<MarketInstrument>,
 }
 
-#[derive(Deserialize, Debug)]
+#[derive(Serialize, Deserialize, Debug)]
 pub struct Order {
     #[serde(rename(serialize = "orderId", deserialize = "orderId"))]
     pub order_id: String,
@@ -106,7 +106,7 @@ pub struct Order {
     pub price: f64,
 }
 
-#[derive(Deserialize, Debug)]
+#[derive(Serialize, Deserialize, Debug)]
 pub struct PlacedLimitOrder {
     #[serde(rename(serialize = "orderId", deserialize = "orderId"))]
     pub order_id: String,
@@ -122,7 +122,7 @@ pub struct PlacedLimitOrder {
     pub commission: Option<MoneyAmount>,
 }
 
-#[derive(Deserialize, Debug)]
+#[derive(Serialize, Deserialize, Debug)]
 pub struct ResponseData<P> {
     #[serde(rename(serialize = "trackingId", deserialize = "trackingId"))]
     pub tracking_id: String,

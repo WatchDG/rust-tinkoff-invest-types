9772349348872eacb91bb957ae12514f8c01aaa3
WatchDG <baranov@w3m.co>
2021-02-20 12:34:43 +0300

    add types: PortfolioPayload, CurrencyPortfolioPayload, PortfolioPosition and CurrencyPortfolioPosition

--- src/lib.rs
+++ src/lib.rs
@@ -13,7 +13,7 @@ pub enum Currency {
     TRY,
 }
 
-#[derive(Serialize, Deserialize, Debug)]
+#[derive(Serialize, Deserialize, Debug, Clone)]
 pub enum InstrumentType {
     Stock,
     Currency,
@@ -21,13 +21,13 @@ pub enum InstrumentType {
     Etf,
 }
 
-#[derive(Serialize, Deserialize, Debug)]
+#[derive(Serialize, Deserialize, Debug, Clone)]
 pub enum OperationType {
     Buy,
     Sell,
 }
 
-#[derive(Serialize, Deserialize, Debug)]
+#[derive(Serialize, Deserialize, Debug, Clone)]
 pub enum OrderStatus {
     New,
     PartiallyFill,
@@ -40,25 +40,25 @@ pub enum OrderStatus {
     PendingNew,
 }
 
-#[derive(Serialize, Deserialize, Debug)]
+#[derive(Serialize, Deserialize, Debug, Clone)]
 pub enum OrderType {
     Limit,
     Market,
 }
 
-#[derive(Serialize, Deserialize, Debug)]
+#[derive(Serialize, Deserialize, Debug, Clone)]
 pub enum BrokerAccountType {
     Tinkoff,
     TinkoffIis,
 }
 
-#[derive(Serialize, Deserialize, Debug)]
+#[derive(Serialize, Deserialize, Debug, Clone)]
 pub struct MoneyAmount {
     pub currency: Currency,
     pub value: f64,
 }
 
-#[derive(Serialize, Deserialize, Debug)]
+#[derive(Serialize, Deserialize, Debug, Clone)]
 pub struct UserAccount {
     #[serde(rename(serialize = "brokerAccountId", deserialize = "brokerAccountId"))]
     pub broker_account_id: String,
@@ -66,12 +66,12 @@ pub struct UserAccount {
     pub broker_account_type: BrokerAccountType,
 }
 
-#[derive(Serialize, Deserialize, Debug)]
+#[derive(Serialize, Deserialize, Debug, Clone)]
 pub struct UserAccountsPayload {
     pub accounts: Vec<UserAccount>,
 }
 
-#[derive(Serialize, Deserialize, Debug)]
+#[derive(Serialize, Deserialize, Debug, Clone)]
 pub struct MarketInstrument {
     pub figi: String,
     pub ticker: String,
@@ -85,12 +85,12 @@ pub struct MarketInstrument {
     pub type_: InstrumentType,
 }
 
-#[derive(Serialize, Deserialize, Debug)]
+#[derive(Serialize, Deserialize, Debug, Clone)]
 pub struct MarketInstrumentsPayload {
     pub instruments: Vec<MarketInstrument>,
 }
 
-#[derive(Serialize, Deserialize, Debug)]
+#[derive(Serialize, Deserialize, Debug, Clone)]
 pub struct Order {
     #[serde(rename(serialize = "orderId", deserialize = "orderId"))]
     pub order_id: String,
@@ -106,7 +106,7 @@ pub struct Order {
     pub price: f64,
 }
 
-#[derive(Serialize, Deserialize, Debug)]
+#[derive(Serialize, Deserialize, Debug, Clone)]
 pub struct PlacedOrder {
     #[serde(rename(serialize = "orderId", deserialize = "orderId"))]
     pub order_id: String,
@@ -122,16 +122,58 @@ pub struct PlacedOrder {
     pub commission: Option<MoneyAmount>,
 }
 
-#[derive(Serialize, Deserialize, Debug)]
+#[derive(Serialize, Deserialize, Debug, Clone)]
 pub struct ErrorPayload {
     pub code: String,
     pub message: String,
 }
 
-#[derive(Serialize, Deserialize, Debug)]
+#[derive(Serialize, Deserialize, Debug, Clone)]
 pub struct ResponseData<P> {
     #[serde(rename(serialize = "trackingId", deserialize = "trackingId"))]
     pub tracking_id: String,
     pub payload: P,
     pub status: String,
+}
+
+#[derive(Serialize, Deserialize, Debug, Clone)]
+pub struct PortfolioPosition {
+    pub figi: String,
+    pub ticker: Option<String>,
+    pub isin: Option<String>,
+    #[serde(rename(serialize = "InstrumentType", deserialize = "InstrumentType"))]
+    pub instrument_type: InstrumentType,
+    pub balance: f64,
+    pub blocked: Option<f64>,
+    #[serde(rename(serialize = "expectedYield", deserialize = "expectedYield"))]
+    pub expected_yield: Option<MoneyAmount>,
+    pub lots: u64,
+    #[serde(rename(
+        serialize = "averagePositionPrice",
+        deserialize = "averagePositionPrice"
+    ))]
+    pub average_position_price: Option<MoneyAmount>,
+    #[serde(rename(
+        serialize = "averagePositionPriceNoNkd",
+        deserialize = "averagePositionPriceNoNkd"
+    ))]
+    pub average_position_price_no_nkd: Option<MoneyAmount>,
+    pub name: String,
+}
+
+#[derive(Serialize, Deserialize, Debug, Clone)]
+pub struct PortfolioPayload {
+    pub positions: Vec<PortfolioPosition>,
+}
+
+#[derive(Serialize, Deserialize, Debug, Clone)]
+pub struct CurrencyPortfolioPosition {
+    pub currency: Currency,
+    pub balance: f64,
+    pub blocked: Option<f64>,
+}
+
+#[derive(Serialize, Deserialize, Debug, Clone)]
+pub struct CurrencyPortfolioPayload {
+    pub currencies: Vec<CurrencyPortfolioPosition>,
 }

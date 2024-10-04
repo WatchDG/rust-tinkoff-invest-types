3d3b898c79299c9a68c2001c1d2621b9697da4cb
WatchDG <baranov@w3m.co>
2024-10-04 22:57:54 +0300

    update deps

--- build.rs
+++ build.rs
@@ -2,7 +2,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     tonic_build::configure()
         .build_server(false)
         .protoc_arg("--experimental_allow_proto3_optional")
-        .compile(
+        .compile_protos(
             &[
                 "contracts-repo/src/docs/contracts/common.proto",
                 "contracts-repo/src/docs/contracts/instruments.proto",
